8c3cb26ba4852dfffd21b21ab527d6be831a9904
Shams Ulzaman <suz250@mun.ca>
2020-03-14 23:39:08 -0230

    Note form functionality

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
 class App{
     constructor(){
+        this.$form = document.querySelector('#form')
+        this.addEventListener()
+        this.$noteTitle = document.querySelector('#note-title')
+        this.$formButton = document.querySelector('#form-buttons')
 
+    }
+
+    addEventListener(){
+        document.body.addEventListener('click', event =>{
+            this.handleFormClick(event)
+            
+        })
+    }
+
+    handleFormClick(event){
+        const isFormClicked = this.$form.contains(event.target)
+
+        if(isFormClicked){
+
+            this.openForm()
+            this.$form.classList.remove('form-open')
+            this.$noteTitle.style.display = 'block'
+            this.$formButton.style.display = 'block'            
+
+        }else{
+
+            this.$form.classList.remove('form-open')
+            this.$noteTitle.style.display = 'none'
+            this.$formButton.style.display = 'none'
+        }
+    }
+
+    openForm(){
+        this.$form.classList.add('form-open')
+        this.$noteTitle.style.display = 'block'
+        this.$formButton.style.display = 'block'
     }
 }
 
-App()
+new App()
